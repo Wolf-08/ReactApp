e4f643df226ea30b7b5b327ed7e7fea9b45c0f6f
Alejandro <alejandro.montecillo@gmail.com>
2022-05-04 10:35:44 -0500

    ✨ FEAT 🔹 Introduction to components and keys of map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from './Button';
+const arr = [
+  'Chanchito feliz',
+  'Chanchito feliz 2',
+  'Chanchito feliz 3',
+  'Chanchito feliz 4',
+]
+//Declarado como una funcion o como una constante
+// con las arrow functions
+// Esto cambia a react creare elemente
+// const Li = ({children}) => {
+//   console.log(children);
+//   return (
+//     <li className='clase-li'>{children}</li>
+//   )
+// }
+// function App() {
+//   const valor = 'Feliz'; // Valores que se pueden imprimir
+//   return (
+//     <ul>
+//       <Li estado={'feliz'}> valor de li</Li>
+//     </ul>
+//   );
+// }
 function App() {
+  const valor = 'Feliz'; // Valores que se pueden imprimir
+  //Renderizado condicional
+  const miVariable = false;
+  // En el click se manda la funcion y eso me puede servir
+  if ( miVariable) {
+    return <p> Mivariable dio true</p>
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1 onClick={(e) => console.log('click', e)}> Hola Mundo</h1>
+    {arr.map(el => <p key={el}>{el}</p>)}
+    <Button onClick= {() => console.log('Click')}>Enviar</Button>
+  </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -11,6 +11,29 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// JSX ES CASI COMO HTML
+// Elementos primitivos de html
+// const x = React.createElement('p', null, 'hola mundo')
+
+// Compoenente funcional que recibe parametros
+// const Li = ({ children, estado, casa, edad}) => {
+//   console.log(casa, edad);
+//   return (
+// <li> {children} estoy {estado}</li>
+//   )}
+// Componente funcional que retorna contenido jsx
+// const Y = () => //Necesita recibir un parametro
+// <ul>
+//   <Li estado = {'feliz'}
+//       casa = {false}
+//       edad={24}> Chanchito</Li>
+//   <Li estado = {' emocionado'}>Chanchito 2</Li>
+//   <Li estado = {'depresion'}>Chanchito 3</Li>
+// </ul>
+// ReactDOM.render(
+// < Y />, document.getElementById('root')
+// )
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
